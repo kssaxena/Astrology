4f4411bcf81bb0d55b8cf21dbebf490dfa8323d5
KSHITIJ SAXENA <133942471+kssaxena@users.noreply.github.com>
2026-06-04 13:42:23 +0530

    feat: initialize backend for astrology project
    - Add package.json with dependencies and scripts for development
    - Create directory for file uploads with .gitKeep
    - Implement app.js as the main application file
    - Set up MongoDB connection in db/index.js
    - Create index.js to configure server and connect to database
    - Add socket.js for real-time communication setup

---
+++ backend/src/index.js
@@ -0,0 +1,24 @@
+import dotenv from "dotenv";
+import http from "http";
+import connectDB from "./db/index.js";
+import { app } from "./app.js";
+import { initSocketServer } from "./socket.js";
+
+dotenv.config({ path: "./env" });
+
+const server = http.createServer(app);
+initSocketServer(server);
+
+connectDB()
+  .then(() => {
+    server.on("error", (err) => {
+      console.log("ERROR", err);
+    });
+    server.listen(process.env.PORT || 8000, () => {
+      console.log("Server port:" + process.env.PORT);
+    });
+  })
+  .catch((err) => {
+    console.log("mongoDB connection failed", err);
+  });
+
